a7d39076aec891115653cde38d14ae9ccce265a3
JuanMa Garcia <juanmgar@gmail.com>
2025-01-31 23:20:38 +0100

    Cambio en el middleware para permitir alguna llamada sin apikey

--- app/middlewares/middlewares.js
+++ app/middlewares/middlewares.js
@@ -1,7 +1,15 @@
-let  authenticationMiddleware  = require("./authenticationMiddleware")
+let authenticationMiddleware = require("./authenticationMiddleware");
 
-let initMiddlewares = (app) =>{
-    app.use(["/transactions/","/creditCards"],authenticationMiddleware)
-}
+let initMiddlewares = (app) => {
+    app.use((req, res, next) => {
+        if (req.path.startsWith("/products/") && req.path !== "/products/categories/count") {
+            return authenticationMiddleware(req, res, next);
+        }
+        next();
+    });
+
+    // Middleware que protege otras rutas sensibles
+    app.use(["/transactions/", "/creditCards"], authenticationMiddleware);
+};
 
 module.exports = initMiddlewares;
